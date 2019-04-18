0c4df4bcde1111f5c89020482e347276efdc872c
Nico95110 <nicolas.lambert95110@gmail.com>
2019-04-18 15:57:05 +0200

    "Commentaires"

--- graphe.h
+++ graphe.h
@@ -1,5 +1,12 @@
 #ifndef GRAPHE_H
 #define GRAPHE_H
+/*!
+* \file graphe.h
+* \brief Stockage du graphe résultant des fichiers d'entrées
+* \author Simon Gautier Nicolas
+* \version 0.5
+*/
+
 #include <string>
 #include <functional>
 #include <queue>
@@ -10,12 +17,33 @@
 #include "svgfile.h"
 #include <limits>
 
+/*! \class graphe
+* \brief classe représentant le graphe
+* La classe gère le stockage et les traitements d'un graphe
+*/
+
 class graphe
 {
     public:
-        ///constructeur qui charge le graphe en mémoire
-        //format du fichier ordre/liste des sommets/taille/liste des arêtes
+        /*!
+        *\brief Constructeur
+
+        * Constructeur de la classe Graphe
+
+        * \param nomFichier : fichier externe de description des sommets
+        * \brief format du nomFichier: ordre/liste des id des sommets avec leurs positions/taille/liste des arêtes avec sommet départ et sommet arrivée
+
+        * \param fichierPoids: fichier externe de description des aretes
+        * \brief format du fichierPoids: taille/nb de pondérations/liste des id des aretes/poids des aretes
+        */
+
         graphe(std::string,std::string);
+
+        /*!
+        * \brief Destructeur
+        * Destructeur de la classe graphe
+        */
+
         ~graphe();
         void afficher() const;
         ///lance un parcours en largeur à partir du sommet d'identifiant id
@@ -26,7 +54,19 @@ class graphe
         void parcoursDFS(std::string) const;
         ///lance et affiche le parcours en profondeur à partir du sommet d'identifiant id
         void afficherDFS(std::string) const;
+
+        /*!
+        * \brief Dessiner
+        * Affichage dans l'output du graphe
+        */
+
         void dessiner(Svgfile& svgout) const;
+
+        /*!
+        * \brief Dessiner Kruskal
+        * Affichage dans l'output de l'arbre couvrant de poids minimum
+        */
+
         void dessinerKruskal(Svgfile& svgout);
         ///recherche et affiche les composantes connexes
         ///retourne le nombre de composantes connexes
@@ -37,19 +77,35 @@ class graphe
         void tri(std::vector<Arete*>& vecteurTri);
         std::vector<Arete*> kruskal();
         std::vector<std::vector<std::string>> compterBinaire();
+
+        /*!
+        * \brief Chemins possibles
+        * Affichage de tous les chemins possibles
+        */
+
         void afficherCasPossible(Svgfile& svgout);
+
+        /*!
+        * \brief Suppression de chemins
+        * Methode qui permet de retirer un cas possible
+        * \param cas_possible : tableau des id des aretes parcourus par le cas étudié
+        * \return true si cas admissible
+        * false sinon
+        */
+
         bool Cas_Admissibles(std::vector<std::string> cas_possible);
         std::vector<float> Dijkstra(int idSommet, std::vector<std::string> casActuel);
         void partie3 (Svgfile& svgout);
 
     protected:
 
-    private:
-        /// Le réseau est constitué d'une collection de sommets
-        std::unordered_map<std::string,Sommet*> m_sommets;//stockée dans une map (clé=id du sommet, valeur= pointeur sur le sommet)
-        std::unordered_map<std::string,Arete*> m_aretes;
-        int m_ordre;
-        int m_taille;
+       private:
+        /// Le réseau est constitué d'une collection de sommets et d'arêtes
+        std::unordered_map<std::string,Sommet*> m_sommets;/*!<Tous les sommets du graphe stockés dans une map (clé=id du sommet, valeur= pointeur sur le sommet)*/
+        std::unordered_map<std::string,Arete*> m_aretes; /*!< Toutes les arêtes du graphe stockés dans une map (clé= id de l'arête, valeur= pointeur sur l'arête)*/
+        int m_ordre; /*!< ordre du graphe*/
+        int m_taille; /*!<taille du graphe*/
+
 };
 
 #endif // GRAPHE_H
--- sommet.h
+++ sommet.h
@@ -1,18 +1,49 @@
 #ifndef SOMMET_H
 #define SOMMET_H
+
+/*!
+* \file sommet.h
+* \brief Stockage du sommet
+* \author Simon Gautier Nicolas
+*\version 1.2
+*/
+
 #include <string>
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
 #include "arete.h"
 
+/*!
+* \class Sommet
+* \brief classe représentant le sommet
+* La classe gère le stockage et les traitements du sommet
+*/
+
 class Sommet
 {
     public:
-        ///constructeur qui reçoit en params les données du sommet
+
+        /*!
+        * \brief Constructeur de la classe Sommet
+        *
+        * \param id : identifiant du sommet
+        *
+        * \param x : position en abscisse du sommet
+        *
+        * \param y : position en ordonnée du sommet
+        */
+
         Sommet(std::string,double,double);
         void ajouterVoisin(Sommet*);
         void afficherData() const;
+
+        /*!
+        * \brief Obtention du degré du sommet
+        * Méthode qui permet d'obtenir le nombre de sommets adjacents au sommet
+        * \return un entier qui correspond au degré du sommet
+        */
+
         int getDegre() const;
         int getX() const;
         int getY() const;
@@ -31,12 +62,13 @@ class Sommet
 
     protected:
 
-    private:
+        private:
         /// Voisinage : liste d'adjacence
-        std::vector<Sommet*> m_voisins;
+        std::vector<Sommet*> m_voisins; /*!< Tous les sommets voisins du sommets sont stockés dans une map (valeur= pointeur sur le sommet voisin)*/
         /// Données spécifiques du sommet
-        std::string m_id; // Identifiant
-        double m_x, m_y; // Position
+        std::string m_id; /*!< Identifiant du sommet*/
+        double m_x, m_y; /*!< Position du sommet */
+
 
 };
 
